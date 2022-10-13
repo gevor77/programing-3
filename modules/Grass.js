1a22f78c7bb90fc0a37f5901f1dd6138a5f60e37
artak2021 <artak.militosyan@tumo.org>
2022-10-13 19:06:47 +0400

    change grass file

--- modules/Grass.js
+++ modules/Grass.js
@@ -3,26 +3,7 @@ var random = require("./random");
 
 
 module.exports = class Grass extends LiveForm {
-    constructor(x, y) {
-        super(x, y);
-        this.multiply = 0;
-    }
-    getNewCoordinates() {
-        this.directions = [
-            [this.x - 1, this.y - 1],
-            [this.x, this.y - 1],
-            [this.x + 1, this.y - 1],
-            [this.x - 1, this.y],
-            [this.x + 1, this.y],
-            [this.x - 1, this.y + 1],
-            [this.x, this.y + 1],
-            [this.x + 1, this.y + 1]
-        ];
-    }
-    chooseCell(character) {
-        this.getNewCoordinates();
-        return super.chooseCell(character);
-    }
+
     mul() {
         this.multiply++;
         let emptyCells = this.chooseCell(0);
--- modules/LiveForm.js
+++ modules/LiveForm.js
@@ -2,6 +2,7 @@ module.exports = class LiveForm {
     constructor(x, y) {
         this.x = x;
         this.y = y;
+        this.multiply = 0;
         this.directions = [
             [this.x - 1, this.y - 1],
             [this.x, this.y - 1],
